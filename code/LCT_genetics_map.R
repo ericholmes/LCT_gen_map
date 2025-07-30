82733aaf3e43cb8b7a9459160828ce9254a4d429
ericholmes <ejholmes@ucdavis.edu>
2025-07-30 12:28:35 -0700

    Added current lakes and rivers and symbolized pleistocene pluvial lakes with crosshatch

--- code/LCT_genetics_map.R
+++ code/LCT_genetics_map.R
@@ -2,13 +2,17 @@
 library(tidyverse)
 library(sf)
 library(nhdplusTools)
-library(ggnewscale)
 library(elevatr)
 library(hillshader)
 library(raster)
 library(terra)
 library(tidyterra)
 library(ggspatial)
+library(ggpattern)
+library(ggnewscale)
+
+Wh
+saveplot = F
 
 # Get US state map data
 us_states <- map_data("state")
@@ -37,11 +41,21 @@ huc8_dissolve <- huc8_all %>%
   summarise(geometry = st_union(geometry)) %>%  # Combine geometries
   st_as_sf()  # Ensure the result is an sf object
 
+huc8_single <- st_union(huc8_all)
+
 # Filter HUC-8 polygons by intersection with Nevada boundary
 huc8_nv <- st_intersection(huc8_all, nevada_sf)
 
+#Load rivers
+nev_nhd <- nhdplusTools::get_nhdplus(AOI = huc8_single, realization = "flowline", streamorder = 4)
+nev_nhd <- st_transform(nev_nhd, st_crs(nevada_sf))
+
+#load lakes
+nev_nhd_lakes <- nhdplusTools::get_waterbodies(AOI = huc8_single)
+nev_nhd_lakes <- st_transform(nev_nhd_lakes, st_crs(nevada_sf))
+
 # Download elevation data using the 'elevatr' package
-elevation_data <- get_elev_raster(locations = nevada_sf, z = 7, prj = st_crs(4326)$proj4string)
+elevation_data <- get_elev_raster(locations = huc8_all, z = 7, prj = st_crs(4326)$proj4string)
 elevation_data <- mask(elevation_data, huc8_dissolve)
 
 # Convert raster to a data frame for ggplot
@@ -52,8 +66,8 @@ colnames(elevation_df) <- c("elevation", "x", "y")
 hs <- hillshader(elevation_data)
 
 ##Save map
-png("output/LCT_map_drape.png",
-          height = 6, width = 4.5, units = "in", res = 1000, family = "serif")
+if(saveplot == T){png("output/LCT_map_drape_rivers_hatch.png",
+          height = 7, width = 5, units = "in", res = 1000, family = "serif")}
 
 ggplot(data = nevada_sf) +
   geom_spatraster(data = rast(hs), show.legend = F, alpha = .5) +
@@ -61,8 +75,18 @@ ggplot(data = nevada_sf) +
   new_scale_fill() +
   geom_tile(data = elevation_df, aes(x = x, y = y, fill = elevation), alpha = .5, show.legend = F) +
   scale_fill_gradientn(colors=terrain.colors(10), name="Elevation (m)", na.value = NA) +
-  geom_sf(data = max_pl_wgs[is.na(max_pl_wgs$LAKENAME) == F, ], fill = "navy", color = NA, alpha = .4) +
-  geom_sf(data = huc8_dissolve, fill = NA, color = "grey40") +
+  # geom_sf(data = max_pl_wgs[is.na(max_pl_wgs$LAKENAME) == F, ], fill = "navy", color = NA, alpha = .4) +
+  geom_sf_pattern(data = max_pl_wgs[is.na(max_pl_wgs$LAKENAME) == F, ], alpha = .3,
+          pattern = "crosshatch",
+          pattern_density = 0.01,       # Adjust density of the pattern
+          pattern_angle = 45,         # Angle of the crosshatch
+          pattern_spacing = 0.005,     # Spacing between lines
+          pattern_fill = "white",     # Color of the pattern
+          pattern_color = "white", fill = NA, pattern_size = .1) +
+  geom_sf(data = nev_nhd_lakes[nev_nhd_lakes$ftype %in% "LakePond",], color = NA, fill = "darkslategrey") +
+  geom_sf(data = nev_nhd, color = "darkslategrey", linewidth = .2) +
+  geom_sf(data = huc8_all, fill = NA, color = "grey40") +
+  geom_sf(data = huc8_dissolve, fill = NA, color = "black") +
   geom_sf(fill = NA, color = "black", linetype = 2) +
   # geom_sf(data = lct_sf) +
   # geom_sf(data = lct_sf_all, aes(color = Basin), size=1) +
@@ -77,5 +101,5 @@ ggplot(data = nevada_sf) +
                          height = unit(0.3,"in"), width = unit(0.3,"in"),
                          pad_x = unit(.02, "in"),pad_y = unit(.02, "in"))
 
-dev.off()
+if(saveplot == T){dev.off()}
 
